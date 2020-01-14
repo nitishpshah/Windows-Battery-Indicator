0ca65fc9fa8524176c6e434e3d5ff6b8e7bb97b8
Nitish Shah <nitishpshah@gmail.com>
2020-01-14 13:24:47 +0530

    Changes to delay, font and charging color
    1. 2 minute update delay instead of 1 second;
    2. Green color when charging and red when battery below 30 percent
    3. Shows remaining time when not charging
    4. Font is now clear and bigger. Can now be comfortable read on hd screens

--- percentage/percentage/TrayIcon.cs
+++ percentage/percentage/TrayIcon.cs
@@ -2,6 +2,8 @@
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace percentage
 {
@@ -10,10 +12,7 @@ namespace percentage
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern bool DestroyIcon(IntPtr handle);
 
-        private const string iconFont = "Segoe UI";
-        private const int iconFontSize = 14;
-
-        private string batteryPercentage;
+        private int batteryPercentage;
         private NotifyIcon notifyIcon;
 
         public TrayIcon()
@@ -29,76 +28,117 @@ namespace percentage
             // initialize menuItem
             menuItem.Index = 0;
             menuItem.Text = "E&xit";
-            menuItem.Click += new System.EventHandler(menuItem_Click);
+            menuItem.Click += new System.EventHandler(MenuItem_Click);
 
             notifyIcon.ContextMenu = contextMenu;
 
-            batteryPercentage = "?";
+            batteryPercentage = 0;
 
             notifyIcon.Visible = true;
 
+			iconUpdate();
+
             Timer timer = new Timer();
-            timer.Tick += new EventHandler(timer_Tick);
-            timer.Interval = 1000; // in miliseconds
-            timer.Start();
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = 2*60*1000; // in miliseconds // Update every 5 minutes
+			// timer.Enabled = true;
+			timer.Start();
         }
 
-        private void timer_Tick(object sender, EventArgs e)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            PowerStatus powerStatus = SystemInformation.PowerStatus;
-            batteryPercentage = (powerStatus.BatteryLifePercent * 100).ToString();
+			iconUpdate();
+		}
 
-            using (Bitmap bitmap = new Bitmap(DrawText(batteryPercentage, new Font(iconFont, iconFontSize), Color.White, Color.Black)))
-            {
-                System.IntPtr intPtr = bitmap.GetHicon();
-                try
-                {
-                    using (Icon icon = Icon.FromHandle(intPtr))
-                    {
-                        notifyIcon.Icon = icon;
-                        notifyIcon.Text = batteryPercentage + "%";
-                    }
-                }
-                finally
-                {
-                    DestroyIcon(intPtr);
-                }
-            }
-        }
+		private void iconUpdate()
+		{
+			PowerStatus powerStatus = SystemInformation.PowerStatus;
+			batteryPercentage = (int)Math.Round((powerStatus.BatteryLifePercent * 100), 0);
 
-        private void menuItem_Click(object sender, EventArgs e)
+			Color foreground = Color.White;
+			BatteryChargeStatus batteryChargeStatus = SystemInformation.PowerStatus.BatteryChargeStatus;
+			bool charging = batteryChargeStatus.HasFlag(BatteryChargeStatus.Charging);
+			if (charging)
+				foreground = Color.FromArgb(255, 0, 255, 0);
+			else if (batteryPercentage < 30) {
+				foreground = Color.FromArgb(255, 255, 0, 0);
+			} else {
+				foreground = Color.FromArgb(255, 255, 255, 255);
+			}
+
+			// Color background = Color.Black;
+			Color background = Color.Transparent;
+
+			string drawMe = batteryPercentage.ToString();
+			Font fontToUse;
+			Brush brushToUse = new SolidBrush(foreground);
+			Rectangle rect;
+			Bitmap bitmapText;
+			Graphics g;
+
+			if (batteryPercentage == 100)
+			{
+				// reduce size to fit "100"
+				fontToUse = new Font("Tahoma", 20, FontStyle.Regular, GraphicsUnit.Pixel);
+			}
+			else
+			{
+				fontToUse = new Font("Tahoma", 28, FontStyle.Regular, GraphicsUnit.Pixel);
+			}
+
+			rect = new Rectangle(-6, 2, 42, 32);
+			bitmapText = new Bitmap(32, 32);
+
+			g = Graphics.FromImage(bitmapText);
+			g.Clear(background);
+
+			using (SolidBrush brush = new SolidBrush(background))
+			{
+				g.FillRectangle(brush, 0, 0, 32, 32);
+			}
+			g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+			StringFormat sf = new StringFormat();
+			sf.Alignment = StringAlignment.Center;
+			sf.LineAlignment = StringAlignment.Center;
+			g.DrawString(drawMe, fontToUse, brushToUse, rect, sf);
+			g.Save();
+
+			System.IntPtr intPtr = bitmapText.GetHicon();
+			try
+			{
+				using (Icon icon = Icon.FromHandle(intPtr))
+				{
+					notifyIcon.Icon = icon;
+
+					if (!charging) {
+						int seconds = SystemInformation.PowerStatus.BatteryLifeRemaining;
+						int mins = seconds / 60;
+						int hours = mins / 60;
+						mins = mins % 60;
+						notifyIcon.Text = hours + ":" + mins + " remaining";
+					} else {
+						notifyIcon.Text = "Charging";
+					}
+				}
+			}
+			finally
+			{
+				DestroyIcon(intPtr);
+			}
+
+
+			//	hIcon = (bitmapText.GetHicon());
+			//	notifyIcon.Icon = System.Drawing.Icon.FromHandle(hIcon);
+			//	notifyIcon.Text = "Remaining " + batteryPercentage.ToString() + "%";
+
+		}
+
+		private void MenuItem_Click(object sender, EventArgs e)
         {
             notifyIcon.Visible = false;
             notifyIcon.Dispose();
             Application.Exit();
         }
 
-        private Image DrawText(String text, Font font, Color textColor, Color backColor)
-        {
-            var textSize = GetImageSize(text, font);
-            Image image = new Bitmap((int) textSize.Width, (int) textSize.Height);
-            using (Graphics graphics = Graphics.FromImage(image))
-            {
-                // paint the background
-                graphics.Clear(backColor);
-
-                // create a brush for the text
-                using (Brush textBrush = new SolidBrush(textColor))
-                {
-                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-                    graphics.DrawString(text, font, textBrush, 0, 0);
-                    graphics.Save();
-                }
-            }
-
-            return image;
-        }
-
-        private static SizeF GetImageSize(string text, Font font)
-        {
-            using (Image image = new Bitmap(1, 1))
-            using (Graphics graphics = Graphics.FromImage(image))
-                return graphics.MeasureString(text, font);
-        }
     }
 }
